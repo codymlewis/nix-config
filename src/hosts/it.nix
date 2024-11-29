9cd50d71a79182c2f20f35350e28a33d35b29d94
Cody Lewis <hello@codymlewis.com>
2024-11-29 22:40:31 +1100

    Removed ssh from laptop

--- src/hosts/it.nix
+++ src/hosts/it.nix
@@ -9,20 +9,6 @@
 
     hardware.bluetooth.enable = true;
 
-    services.openssh = {
-        enable = false;
-        ports = [ 22 ];
-        settings = {
-            PasswordAuthentication = false;
-            AllowUsers = [ "cody" ];
-            UseDns = false;
-            X11Forwarding = false;
-            PermitRootLogin = "no";
-        };
-    };
-
-    networking.firewall.allowedTCPPorts = [ 22 ];
-
     system.autoUpgrade.flake = "/etc/nixos#it";
 
 }
